75eee4e9a4483a2dabf8b8baed4d0b7287241a00
Karianne <113008581+kariannegle@users.noreply.github.com>
2024-09-11 22:07:47 +0200

    Egen post controller

--- obj/Debug/net8.0/NotesImageSharingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/NotesImageSharingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotesImageSharingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e54bf2eae628aa6a187e62680b2eced5068556c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15aed7972327ee774f66bef50d80822a977ea82f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotesImageSharingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotesImageSharingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
